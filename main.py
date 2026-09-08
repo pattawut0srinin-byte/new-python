f7095061fdccb7d00f552b8c67000bd669f04c27
pattawut0srinin-byte <pattawut0srinin@gmail.com>
2026-09-08 16:19:07 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-print("Hello")
-print("Information and Network Engineering")
-print("pattawut you will be great")
-
-print("I'm hungry")
-print('He ask me "who got the books?"')
-
-print("I'm good \"I will do my best\"")
-print("/\\")
